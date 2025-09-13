f452bf48b7eb293ae703c9018af0b8c57a1d2da4
chanchaltiwari01 <chanchaltiwari2001@gmail.com>
2025-09-13 10:46:11 +0530

    change linux to window

---
+++ dictionary.py
@@ -0,0 +1,12 @@
+# Empty dictionary
+my_dict = {}
+
+# Dictionary with data
+person = {
+    "name": "Chanchal",
+    "age": 25,
+    "city": "Bhopal"
+}
+print (person)
+print(type(person))
+print(my_dict)
